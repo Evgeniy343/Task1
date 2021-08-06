581ff69897c8e1a24df86ebe348ac26861672dda
EVGENIY <churaievgeniy2001@mail.ru>
2021-08-06 20:50:44 +0300

    fdgd

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -8,16 +8,26 @@ import org.apache.logging.log4j.Logger;
 public class Main {
     private final static String PATH = "D:\\epam\\projects\\Task1\\data\\File_1.txt";
     private final static GeometryActionFactory factory = GeometryActionFactory.newInstance();
-    private static final Logger LOG = LogManager.getLogger();
+    private static final Logger LOG = LogManager.getLogger(Main.class);
 
     public static void main(String[] args) {
         ReaderFileWork reader = new ReaderFileWork();
         CalculationParams params;
         try {
             params = new CalculationParams(reader.readingFromFile(PATH),null);
+
+
+
+            System.out.println(LOG.isErrorEnabled());
+
+
             LOG.info(factory.createAction(CubeAction.CUBE_AREA).execute(params).getArea());
-            LOG.error(factory.createAction(CubeAction.CUBE_VOLUME).execute(params).getVolume());
-            LOG.error(factory.createAction(CubeAction.CUBE_BASE_ON_PLANE).execute(params).isBaseOnPlane());
+            LOG.info(factory.createAction(CubeAction.CUBE_AREA).execute(params).getArea());
+            LOG.info(factory.createAction(CubeAction.CUBE_AREA).execute(params).getArea());
+            LOG.info(factory.createAction(CubeAction.CUBE_AREA).execute(params).getArea());
+            LOG.info(factory.createAction(CubeAction.CUBE_AREA).execute(params).getArea());
+
+
         } catch (Exception e) {
             e.printStackTrace();
         }
